0e1e910d51802b0d01232d432f21447d8ea0dd4d
reza72rg <reza72rg@gmail.com>
2024-07-04 19:33:23 +0330

    Create custom user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from user.models import User, Profile
 
-# Register your models here.
+
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_active", "is_verified")
+    list_filter = ("email", "is_staff", "is_active", "is_verified")
+    searching_fileds = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ("Main", {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                )
+            },
+        ),
+        ("group permissions", {"fields": ("groups", "user_permissions")}),
+        ("important date", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # Specify the fields to be displayed on the change form
+    def get_fields(self, request, obj=None):
+        return ["name", "user", "phone_number", "age", "gender"]
+
+    # Specify the fields to be displayed on the change list
+    def get_list_display(self, request):
+        return ["user", "name", "phone_number", "age", "gender"]
+
+
