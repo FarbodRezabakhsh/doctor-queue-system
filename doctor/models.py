d2cec3704c6d3ca74044c101924c3a82ac88dd0e
farbod <farbod.rezabakhsh81@gmail.com>
2024-07-05 00:11:53 +0330

    create models.py for Doctor's app.

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models this.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    resident = models.CharField(max_length=100)
+    info_text = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    visit_time = models.DateTimeField()
+    location_text = models.CharField(max_length=250)
+    visit_prise = models.DecimalField(max_digits=10)
+
+    def __str__(self):
+        return f'{self.user} will be visited at {self.visit_time} '
+class WorkTable(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    time_table = models.JSONField()
+    visit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class patient_info(models.Model):
+    user = models.ManyToManyField(User, on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    medical_history = models.TextField()
+    drug_history = models.TextField()
