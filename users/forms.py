efef9e7175c5562f22da59d6eb50b3e43b96fd49
Dr.hossein shabani (Mehran) <shabanimehran@gmail.com>
2024-07-10 19:36:23 +0330

    Update form fields and styles in signup
    The form fields in users/forms.py and corresponding styles in signup.css have been updated to improve the signup form. The help texts for form fields were removed and classes added to enable better control over their styles. The signup.html template also had a line modified.

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,5 @@
+# forms.py
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
@@ -7,3 +9,8 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email', 'phone_number', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = None
