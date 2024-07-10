3d322d135311b20118c0834817314c42dfd1d456
reza72rg <reza72rg@gmail.com>
2024-07-10 22:39:26 +0330

    delete doctors.models

--- doctor/models.py
+++ doctor/models.py
@@ -1,10 +1,2 @@
 from django.db import models
 
-# Create your models this.
-
-
-class Doctor(models.Model):
-    doctor = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.doctor
